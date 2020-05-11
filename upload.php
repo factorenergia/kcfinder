7d9faa5e12fd7d480199bea2239003a22c41a678
Juan Salgado <jsalgado@factorenergia.com>
2020-05-11 16:06:22 +0200

    - Release to usage

--- upload.php
+++ upload.php
@@ -9,5 +9,6 @@
 
 require "core/bootstrap.php";
 $uploader = "kcfinder\\uploader";  // To execute core/bootstrap.php on older
+
 $uploader = new $uploader();       // PHP versions (even PHP 4)
 $uploader->upload();
